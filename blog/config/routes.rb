68f21355140d691224c0058fac06693a06a4f738
mostafa-anm <mostafa.anm91@gmail.com>
2014-05-21 12:06:48 +0200

    Corrected

--- blog/config/routes.rb
+++ blog/config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
-  root 'welcome#index'
+  get 'welcome/index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+   #root 'welcome#index'
+   Blog::Application.routes.draw do
+    resources :articles
+
+    root 'welcome#index'  
+  end
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -54,4 +60,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
 end
